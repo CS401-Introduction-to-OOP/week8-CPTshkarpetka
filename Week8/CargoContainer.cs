8be98e3303aee4cb0902beacdb5792c52c34de29
kat0d1 <vasukmikola728@gmail.com>
2026-03-28 14:54:25 +0200

    Created CargoContainer class

---
+++ Week8/CargoContainer.cs
@@ -0,0 +1,16 @@
+﻿namespace Week8;
+
+public class CargoContainer<T> where T: DeliveryItem
+{
+    private List<T> Items = new();
+
+    public void AddItem(T item)
+    {
+        Items.Add(item);
+    }
+
+    public double GetTotalCost()
+    {
+        return Items.Sum(item => item.CalculateCost());
+    }
+}
