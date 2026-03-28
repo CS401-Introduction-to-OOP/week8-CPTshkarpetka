a563b1ebe0a9b95d651867c65905d661612e3643
kat0d1 <vasukmikola728@gmail.com>
2026-03-28 14:59:13 +0200

    Created Program.cs

--- Week8/Program.cs
+++ Week8/Program.cs
@@ -1,3 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Week8;
 
-Console.WriteLine("Hello, World!");
+DeliveryItem letter1 = new Letter("letter1", 100);
+DeliveryItem letter2 = new Letter("letter2", 10);
+DeliveryItem parcel1 = new Parcel("parcel1", 5, "10x10x50");
+DeliveryItem parcel2 = new Parcel("parcel2", 10, "10x20x30");
+
+letter1.PrintInfo();
+letter2.PrintInfo();
+parcel1.PrintInfo();
+parcel2.PrintInfo();
+
+CargoContainer<DeliveryItem> myCargo = new CargoContainer<DeliveryItem>();
+
+myCargo.AddItem(letter1);
+myCargo.AddItem(letter2);
+myCargo.AddItem(parcel1);
+myCargo.AddItem(parcel2);
+
+Console.WriteLine($"Total cost: {myCargo.GetTotalCost()}");
